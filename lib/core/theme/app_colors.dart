bd7d5fc6c0d942b28e0cb8fb4b38318aad035b34
ahmed <ahmedhosni803@gmail.com>
2024-12-20 18:26:59 +0200

    add hadeth screen

--- lib/core/theme/app_colors.dart
+++ lib/core/theme/app_colors.dart
@@ -4,4 +4,5 @@ class AppColors {
   static const Color white = Color((0xffFFFFFF));
   static const Color black = Color((0xff212121));
   static const Color whiteCoffee = Color((0xffE2BE7F));
+  static const Color newColor = Color((0xffE2BE7F));
 }
